09928de249132c5d8c90e9aed63aa79e86b77c8e
2jc26 <javier.cerino.p@gmail.com>
2021-02-01 09:56:55 -0500

    Se realizaron los cambios en el archivo view especificados en el laboratorio 1

--- App/view.py
+++ App/view.py
@@ -42,7 +42,7 @@ def printMenu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
-    # TO-DO: Modificaciones para completar el laboratorio 1.
+    print("3- Cargar Tags de los Libros")
     print("0- Salir")
 
 
@@ -60,6 +60,13 @@ def loadTags():
     return controller.loadTags('GoodReads/tags.csv')
 
 
+def loadBooksTags():
+    """
+    Carga los Tags de los Libros
+    """
+    return controller.loadBooksTags('GoodReads/book_tags-small.csv')
+
+
 """
 Menu principal
 """
@@ -75,7 +82,11 @@ while True:
         print("Cargando información de tags....")
         tags = loadTags()
         print('Total de tags cargados: ' + str(lt.size(tags)))
-    # TO-DO: Modificaciones para completar el laboratorio 1.
+    
+    elif int(inputs[0]) == 3:
+        print("Cargando información de tags de libros....")
+        book_tags = loadBooksTags()
+        print('Total de tags cargados: ' + str(lt.size(book_tags)))
 
     else:
         sys.exit(0)
